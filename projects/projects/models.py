55e4dbfd21fee27661f1e8db36ae54c27cae0740
vesloguzov <vesloguzov@gmail.com>
2015-01-29 19:46:16 +0500

    First commit

--- projects/projects/models.py
+++ projects/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField('Название проекта', max_length=1024)
+    pub_date=models.DateTimeField(auto_now_add=True)
+    comment=models.TextField('Комментарий', max_length=4096, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'проект'
+        verbose_name_plural = 'проекты'
--- projects/settings.py
+++ projects/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'projects.projects',
+	'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (
